404bba77c9934503f7b33aeb58d220ba671154b1
Abhijeet Singh <contact.abhijeetsingh@gmail.com>
2020-10-23 22:19:59 +0530

    Fix image alt attributes (#3)

--- src/Components/About/ImageCarousel.js
+++ src/Components/About/ImageCarousel.js
@@ -1,38 +1,42 @@
 import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
-import './ImageCarousel.css'
+import { Carousel } from "react-responsive-carousel";
+import "./ImageCarousel.css";
 
 class ImageCarousel extends React.Component {
-    render() {
-        return (
-            <div>
-                <section id="about">
-                <div className="row">
-                <Carousel autoPlay interval="5000" transitionTime="5000" position="center" top="108px" left="108px" width="1000px" height= "700px" >
-                    <div>
-                        <img src="https://i.ibb.co/Tv861RB/1.png" />
-                        
-                    </div>
-                    <div>
-                        <img src="https://i.ibb.co/VDXQGrK/2.png" />
-                        
-                    </div>
-                    <div>
-                        <img src="https://i.ibb.co/q1QHCrC/3.png" />
-                        
-                    </div>
-                    <div>
-                        <img src="https://i.ibb.co/8xN9qQZ/4.png" />
-                        
-                    </div>
-                </Carousel>
-                </div>
-                </section>
-                
-            </div>
-        )
-    };
+  render() {
+    return (
+      <div>
+        <section id="about">
+          <div className="row">
+            <Carousel
+              autoPlay
+              interval="5000"
+              transitionTime="5000"
+              position="center"
+              top="108px"
+              left="108px"
+              width="1000px"
+              height="700px"
+            >
+              <div>
+                <img src="https://i.ibb.co/Tv861RB/1.png" alt="" />
+              </div>
+              <div>
+                <img src="https://i.ibb.co/VDXQGrK/2.png" alt="" />
+              </div>
+              <div>
+                <img src="https://i.ibb.co/q1QHCrC/3.png" alt="" />
+              </div>
+              <div>
+                <img src="https://i.ibb.co/8xN9qQZ/4.png" alt="" />
+              </div>
+            </Carousel>
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default ImageCarousel;
