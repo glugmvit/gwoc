11c32cfa99735d016e76bc3b4a91c90530f362a3
aa-deet-eeya <aditya99mondal@gmail.com>
2020-10-24 13:57:45 +0530

    made the about us section mobile responsive

--- src/Components/About/ImageCarousel.js
+++ src/Components/About/ImageCarousel.js
@@ -9,26 +9,22 @@ class ImageCarousel extends React.Component {
         return (
             <div>
                 <section id="about">
-                <div className="row">
-                <Carousel autoPlay interval="5000" transitionTime="5000" position="center" top="108px" left="108px" width="1000px" height= "700px" >
-                    <div>
-                        <img src="https://i.ibb.co/Tv861RB/1.png" alt="image 1" />
-                        
-                    </div>
-                    <div>
-                        <img src="https://i.ibb.co/VDXQGrK/2.png" alt="image 2" />
-                        
-                    </div>
-                    <div>
-                        <img src="https://i.ibb.co/q1QHCrC/3.png" alt="image 3"/>
-                        
-                    </div>
-                    <div>
-                        <img src="https://i.ibb.co/8xN9qQZ/4.png" alt="image4"/>
-                        
-                    </div>
-                </Carousel>
-                </div>
+                    <center>
+                        <Carousel autoPlay interval="2000" transitionTime="1000" position="center" width="90%" >
+                        <div>
+                            <img src="https://i.ibb.co/Tv861RB/1.png" alt="image 1" />
+                        </div>
+                        <div>
+                            <img src="https://i.ibb.co/VDXQGrK/2.png" alt="image 2" />
+                        </div>
+                        <div>
+                            <img src="https://i.ibb.co/q1QHCrC/3.png" alt="image 3"/>
+                        </div>
+                        <div>
+                            <img src="https://i.ibb.co/8xN9qQZ/4.png" alt="image4"/>
+                        </div>
+                        </Carousel>
+                    </center>
                 </section>
                 
             </div>
