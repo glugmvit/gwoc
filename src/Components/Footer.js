ffede33f83ecc13ea04ad27f8a71d1cb21e036dc
aa-deet-eeya <aditya99mondal@gmail.com>
2020-10-25 20:20:18 +0530

    re-added the updated prettier format

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,27 +2,25 @@ import React, { Component } from 'react';
 
 class Footer extends Component {
   render() {
-
-    
-
     return (
       <footer>
-
-     <div className="row">
-        <div className="twelve columns">
-           {/* <ul className="social-links">
+        <div className="row">
+          <div className="twelve columns">
+            {/* <ul className="social-links">
               {networks}
            </ul> */}
 
-           <ul className="copyright">
+            <ul className="copyright">
               <li>&copy; Copyright 2020 GLUG MVIT</li>
-              
-           </ul>
-
+            </ul>
+          </div>
+          <div id="go-top">
+            <a className="smoothscroll" title="Back to Top" href="#home">
+              <i className="icon-up-open"></i>
+            </a>
+          </div>
         </div>
-        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
-     </div>
-  </footer>
+      </footer>
     );
   }
 }
