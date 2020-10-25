56a51bf65b4a1bcc79c1544cfdba5d5fbf1aa1dc
aa-deet-eeya <aditya99mondal@gmail.com>
2020-10-25 19:42:45 +0530

    Revert "prettier format"
    This reverts commit dc31e7938ea4b2eac76334ec1fa31552135a8914.

--- public/js/init.js
+++ public/js/init.js
@@ -4,171 +4,185 @@
 /*
 -----------------------------------------------------------------------------------*/
 
-jQuery(document).ready(function ($) {
-  /*----------------------------------------------------*/
-  /* FitText Settings
+ jQuery(document).ready(function($) {
+
+/*----------------------------------------------------*/
+/* FitText Settings
 ------------------------------------------------------ */
 
-  setTimeout(function () {
-    $("h1.responsive-headline").fitText(1, {
-      minFontSize: "40px",
-      maxFontSize: "90px",
-    });
-  }, 100);
+    setTimeout(function() {
+	   $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
+	 }, 100);
 
-  /*----------------------------------------------------*/
-  /* Smooth Scrolling
+
+/*----------------------------------------------------*/
+/* Smooth Scrolling
 ------------------------------------------------------ */
 
-  $(".smoothscroll").on("click", function (e) {
-    e.preventDefault();
+   $('.smoothscroll').on('click',function (e) {
+	    e.preventDefault();
 
-    var target = this.hash,
-      $target = $(target);
+	    var target = this.hash,
+	    $target = $(target);
 
-    $("html, body")
-      .stop()
-      .animate(
-        {
-          scrollTop: $target.offset().top,
-        },
-        800,
-        "swing",
-        function () {
-          window.location.hash = target;
-        }
-      );
-  });
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 800, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
 
-  /*----------------------------------------------------*/
-  /* Highlight the current section in the navigation bar
+
+/*----------------------------------------------------*/
+/* Highlight the current section in the navigation bar
 ------------------------------------------------------*/
 
-  var sections = $("section");
-  var navigation_links = $("#nav-wrap a");
+	var sections = $("section");
+	var navigation_links = $("#nav-wrap a");
 
-  sections.waypoint({
-    handler: function (event, direction) {
-      var active_section;
+	sections.waypoint({
 
-      active_section = $(this);
-      if (direction === "up") active_section = active_section.prev();
+      handler: function(event, direction) {
 
-      var active_link = $(
-        '#nav-wrap a[href="#' + active_section.attr("id") + '"]'
-      );
+		   var active_section;
 
-      navigation_links.parent().removeClass("current");
-      active_link.parent().addClass("current");
-    },
-    offset: "35%",
-  });
+			active_section = $(this);
+			if (direction === "up") active_section = active_section.prev();
 
-  /*----------------------------------------------------*/
-  /*	Make sure that #header-background-image height is
+			var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');
+
+         navigation_links.parent().removeClass("current");
+			active_link.parent().addClass("current");
+
+		},
+		offset: '35%'
+
+	});
+
+
+/*----------------------------------------------------*/
+/*	Make sure that #header-background-image height is
 /* equal to the browser height.
 ------------------------------------------------------ */
 
-  $("header").css({ height: $(window).height() });
-  $(window).on("resize", function () {
-    $("header").css({ height: $(window).height() });
-    $("body").css({ width: $(window).width() });
-  });
+   $('header').css({ 'height': $(window).height() });
+   $(window).on('resize', function() {
 
-  /*----------------------------------------------------*/
-  /*	Fade In/Out Primary Navigation
-------------------------------------------------------*/
+        $('header').css({ 'height': $(window).height() });
+        $('body').css({ 'width': $(window).width() })
+   });
 
-  $(window).on("scroll", function () {
-    var h = $("header").height();
-    var y = $(window).scrollTop();
-    var nav = $("#nav-wrap");
 
-    if (y > h * 0.2 && y < h && $(window).outerWidth() > 768) {
-      nav.fadeOut("fast");
-    } else {
-      if (y < h * 0.2) {
-        nav.removeClass("opaque").fadeIn("fast");
-      } else {
-        nav.addClass("opaque").fadeIn("fast");
-      }
-    }
-  });
-
-  /*----------------------------------------------------*/
-  /*	Modal Popup
-------------------------------------------------------*/
-
-  $(".item-wrap a").magnificPopup({
-    type: "inline",
-    fixedContentPos: false,
-    removalDelay: 200,
-    showCloseBtn: false,
-    mainClass: "mfp-fade",
-  });
-
-  $(document).on("click", ".popup-modal-dismiss", function (e) {
-    e.preventDefault();
-    $.magnificPopup.close();
-  });
-
-  /*----------------------------------------------------*/
-  /*	Flexslider
 /*----------------------------------------------------*/
-  $(".flexslider").flexslider({
-    namespace: "flex-",
-    controlsContainer: ".flex-container",
-    animation: "slide",
-    controlNav: true,
-    directionNav: false,
-    smoothHeight: true,
-    slideshowSpeed: 7000,
-    animationSpeed: 600,
-    randomize: false,
-  });
-
-  /*----------------------------------------------------*/
-  /*	contact form
+/*	Fade In/Out Primary Navigation
 ------------------------------------------------------*/
 
-  $("form#contactForm button.submit").click(function () {
-    $("#image-loader").fadeIn();
+   $(window).on('scroll', function() {
 
-    var contactName = $("#contactForm #contactName").val();
-    var contactEmail = $("#contactForm #contactEmail").val();
-    var contactSubject = $("#contactForm #contactSubject").val();
-    var contactMessage = $("#contactForm #contactMessage").val();
+		var h = $('header').height();
+		var y = $(window).scrollTop();
+      var nav = $('#nav-wrap');
 
-    var data =
-      "contactName=" +
-      contactName +
-      "&contactEmail=" +
-      contactEmail +
-      "&contactSubject=" +
-      contactSubject +
-      "&contactMessage=" +
-      contactMessage;
+	   if ( (y > h*.20) && (y < h) && ($(window).outerWidth() > 768 ) ) {
+	      nav.fadeOut('fast');
+	   }
+      else {
+         if (y < h*.20) {
+            nav.removeClass('opaque').fadeIn('fast');
+         }
+         else {
+            nav.addClass('opaque').fadeIn('fast');
+         }
+      }
 
-    $.ajax({
-      type: "POST",
-      url: "inc/sendEmail.php",
-      data: data,
-      success: function (msg) {
-        // Message was sent
-        if (msg == "OK") {
-          $("#image-loader").fadeOut();
-          $("#message-warning").hide();
-          $("#contactForm").fadeOut();
-          $("#message-success").fadeIn();
-        }
-        // There was an error
-        else {
-          $("#image-loader").fadeOut();
-          $("#message-warning").html(msg);
-          $("#message-warning").fadeIn();
-        }
-      },
+	});
+
+
+/*----------------------------------------------------*/
+/*	Modal Popup
+------------------------------------------------------*/
+
+    $('.item-wrap a').magnificPopup({
+
+       type:'inline',
+       fixedContentPos: false,
+       removalDelay: 200,
+       showCloseBtn: false,
+       mainClass: 'mfp-fade'
+
     });
-    return false;
-  });
+
+    $(document).on('click', '.popup-modal-dismiss', function (e) {
+    		e.preventDefault();
+    		$.magnificPopup.close();
+    });
+
+
+/*----------------------------------------------------*/
+/*	Flexslider
+/*----------------------------------------------------*/
+   $('.flexslider').flexslider({
+      namespace: "flex-",
+      controlsContainer: ".flex-container",
+      animation: 'slide',
+      controlNav: true,
+      directionNav: false,
+      smoothHeight: true,
+      slideshowSpeed: 7000,
+      animationSpeed: 600,
+      randomize: false,
+   });
+
+/*----------------------------------------------------*/
+/*	contact form
+------------------------------------------------------*/
+
+   $('form#contactForm button.submit').click(function() {
+
+      $('#image-loader').fadeIn();
+
+      var contactName = $('#contactForm #contactName').val();
+      var contactEmail = $('#contactForm #contactEmail').val();
+      var contactSubject = $('#contactForm #contactSubject').val();
+      var contactMessage = $('#contactForm #contactMessage').val();
+
+      var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
+               '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
+
+      $.ajax({
+
+	      type: "POST",
+	      url: "inc/sendEmail.php",
+	      data: data,
+	      success: function(msg) {
+
+            // Message was sent
+            if (msg == 'OK') {
+               $('#image-loader').fadeOut();
+               $('#message-warning').hide();
+               $('#contactForm').fadeOut();
+               $('#message-success').fadeIn();   
+            }
+            // There was an error
+            else {
+               $('#image-loader').fadeOut();
+               $('#message-warning').html(msg);
+	            $('#message-warning').fadeIn();
+            }
+
+	      }
+
+      });
+      return false;
+   });
+
+
 });
+
+
+
+
+
+
+
+
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,26 +1,28 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Footer extends Component {
   render() {
+
+    
+
     return (
       <footer>
-        <div className="row">
-          <div className="twelve columns">
-            {/* <ul className="social-links">
+
+     <div className="row">
+        <div className="twelve columns">
+           {/* <ul className="social-links">
               {networks}
            </ul> */}
 
-            <ul className="copyright">
+           <ul className="copyright">
               <li>&copy; Copyright 2020 GLUG MVIT</li>
-            </ul>
-          </div>
-          <div id="go-top">
-            <a className="smoothscroll" title="Back to Top" href="#home">
-              <i className="icon-up-open"></i>
-            </a>
-          </div>
+              
+           </ul>
+
         </div>
-      </footer>
+        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
+     </div>
+  </footer>
     );
   }
 }
