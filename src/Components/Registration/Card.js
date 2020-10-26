3025a54ab8f0afa0e4b8e437587178fe21238144
Prakruthi <prakruthim25@gmail.com>
2020-10-26 12:20:58 +0530

    conflicts in card and timeline resolved

--- src/Components/Registration/Card.js
+++ src/Components/Registration/Card.js
@@ -2,25 +2,25 @@ import React from 'react';
 import './Card.css';
 
 
-class Card extends React.Component{
-    render(){
-        return(
-                <div>
+// class Card extends React.Component{
+//     render(){
+//         return(
+//                 <div>
 
-                  <section id="register">
+//                   <section id="register">
 
               
-                  <div className="row banner">
-                  <div className="banner-text">
-                  <h1 className="responsive-headline" style={{textAlignVertical: "center",textAlign: "center",}}>REGISTER</h1>
-                  </div>
+//                   <div className="row banner">
+//                   <div className="banner-text">
+//                   <h1 className="responsive-headline" style={{textAlignVertical: "center",textAlign: "center",}}>REGISTER</h1>
+//                   </div>
                   
-                        <br></br>
-      <div class="example-2 card">
-        <div class="wrapper">
-          <div class="header">
+//                         <br></br>
+//       <div class="example-2 card">
+//         <div class="wrapper">
+//           <div class="header">
             
-            <ul class="menu-content">
+//             <ul class="menu-content">
               
 
 class Card extends React.Component {
@@ -64,8 +64,7 @@ class Card extends React.Component {
             <div class="data">
               <div class="content">
                 <a
-                  href="https://docs.google.com/forms/d/e/1FAIpQLScsD1HoShQei19Hli0DzZUGH3ZxMvsA1h_a9ukHn53jEX4EHw/viewform?usp=sf_link
-"
+                  href="https://docs.google.com/forms/d/e/1FAIpQLScsD1HoShQei19Hli0DzZUGH3ZxMvsA1h_a9ukHn53jEX4EHw/viewform?usp=sf_link"
                   class="button"
                 >
                   Register
@@ -77,15 +76,16 @@ class Card extends React.Component {
 
       </div> 
        
-            </div>
-            </section>
-            </div>
+            // </div>
+            // </section>
+            // </div>
         );
     }
 
-      </div>
-    );
+      // </div>
+   " );"
   }
 
-}
+
 export default Card;
+
