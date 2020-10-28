e92d17f0edcdf2ebb0ac401a41fce5f8e47b3a10
Shreya Raj <shreyaguddaraj@gmail.com>
2020-10-28 12:46:16 +0530

    formatted using prettier

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -10,22 +10,24 @@ class Footer extends Component {
               {networks}
            </ul> */}
 
-
-           <ul className="copyright">
-              <li>&copy; Copyright 2020 <a href="https://blog.glugmvit.com/" target="_blank" rel="noopener noreferrer">GLUG MVIT</a> </li>
-              
-              
-           </ul>
-
-
-           
+            <ul className="copyright">
+              <li>
+                &copy; Copyright 2020{' '}
+                <a
+                  href="https://blog.glugmvit.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GLUG MVIT
+                </a>{' '}
+              </li>
+            </ul>
           </div>
           <div id="go-top">
             <a className="smoothscroll" title="Back to Top" href="#home">
               <i className="icon-up-open"></i>
             </a>
           </div>
-
         </div>
       </footer>
     );
--- src/Components/Schedule/Timeline.js
+++ src/Components/Schedule/Timeline.js
@@ -1,113 +1,98 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import 'react-vertical-timeline-component/style.min.css';
-import './Timeline.css'
+import './Timeline.css';
 // Timeline vertical
 class Timeline extends Component {
   render() {
     return (
-      <div className="row banner" id="schedule"
-      style={{ paddingTop: '80px'}}
-      >
-                  <div className="banner-text">
-                  <h1 className="responsive-headline" style={{textAlignVertical: "center",textAlign: "center",}}>SCHEDULE</h1>
-                  </div>
-                  
-                        <br></br>
+      <div className="row banner" id="schedule" style={{ paddingTop: '80px' }}>
+        <div className="banner-text">
+          <h1
+            className="responsive-headline"
+            style={{ textAlignVertical: 'center', textAlign: 'center' }}
+          >
+            SCHEDULE
+          </h1>
+        </div>
 
-<section id="cd-timeline" class="cd-container">
-    <div class="cd-timeline-block">
-      <div class="cd-timeline-img cd-picture">
+        <br></br>
+
+        <section id="cd-timeline" class="cd-container">
+          <div class="cd-timeline-block">
+            <div class="cd-timeline-img cd-picture"></div>
+
+            <div class="cd-timeline-content">
+              <h3 style={{ color: 'white' }}>Event 1</h3>
+              <div class="timeline-content-info">
+                <span class="timeline-content-info-date">
+                  <i class="fa fa-calendar-o" aria-hidden="true"></i>
+                  Nov 2020 - Present
+                </span>
+              </div>
+              <h2>desc</h2>
+            </div>
+          </div>
+
+          <div class="cd-timeline-block">
+            <div class="cd-timeline-img cd-movie"></div>
+
+            <div class="cd-timeline-content">
+              <h3 style={{ color: 'white' }}>Event 2</h3>
+              <div class="timeline-content-info">
+                <span class="timeline-content-info-date">
+                  <i class="fa fa-calendar-o" aria-hidden="true"></i>
+                  Nov 2020 - Present
+                </span>
+              </div>
+              <h2>desc</h2>
+            </div>
+          </div>
+
+          <div class="cd-timeline-block">
+            <div class="cd-timeline-img cd-picture"></div>
+
+            <div class="cd-timeline-content">
+              <h3 style={{ color: 'white' }}>Event 3</h3>
+              <div class="timeline-content-info">
+                <span class="timeline-content-info-date">
+                  <i class="fa fa-calendar-o" aria-hidden="true"></i>
+                  Nov 2020 - Present
+                </span>
+              </div>
+              <h2>desc</h2>
+            </div>
+          </div>
+          <div class="cd-timeline-block">
+            <div class="cd-timeline-img cd-location"></div>
+
+            <div class="cd-timeline-content">
+              <h3 style={{ color: 'white' }}>Event 4</h3>
+              <div class="timeline-content-info">
+                <span class="timeline-content-info-date">
+                  <i class="fa fa-calendar-o" aria-hidden="true"></i>
+                  Nov 2020 - Present
+                </span>
+              </div>
+              <h2>desc</h2>
+            </div>
+          </div>
+
+          <div class="cd-timeline-block">
+            <div class="cd-timeline-img cd-location"></div>
+
+            <div class="cd-timeline-content">
+              <h3 style={{ color: 'white' }}>Event 5</h3>
+              <div class="timeline-content-info">
+                <span class="timeline-content-info-date">
+                  <i class="fa fa-calendar-o" aria-hidden="true"></i>
+                  Nov 2020 - Present
+                </span>
+              </div>
+              <h2>desc</h2>
+            </div>
+          </div>
+        </section>
       </div>
-
-      <div class="cd-timeline-content">
-        <h3 style={{ color: 'white' }}>Event 1</h3>
-        <div class="timeline-content-info">
-          
-          <span class="timeline-content-info-date">
-            <i class="fa fa-calendar-o" aria-hidden="true"></i>
-            Nov 2020 - Present
-          </span>
-        </div>
-        <h2>desc</h2>
-        
-      </div> 
-    </div> 
-
-    <div class="cd-timeline-block">
-      <div class="cd-timeline-img cd-movie">
-      </div> 
-
-      <div class="cd-timeline-content">
-                
-            <h3 style={{ color: 'white' }}>Event 2</h3>
-                <div class="timeline-content-info">
-          
-          <span class="timeline-content-info-date">
-            <i class="fa fa-calendar-o" aria-hidden="true"></i>
-            Nov 2020 - Present
-          </span>
-        </div>
-        <h2>desc</h2>
-        
-      </div> 
-    </div> 
-
-    <div class="cd-timeline-block">
-      <div class="cd-timeline-img cd-picture">
-      </div>
-
-      <div class="cd-timeline-content">
-            <h3 style={{ color: 'white' }}>Event 3</h3>
-            <div class="timeline-content-info">
-          
-          <span class="timeline-content-info-date">
-            <i class="fa fa-calendar-o" aria-hidden="true"></i>
-            Nov 2020 - Present
-          </span>
-        </div>
-        <h2>desc</h2>
-        
-      </div> 
-    </div> 
-    <div class="cd-timeline-block">
-      <div class="cd-timeline-img cd-location">
-      </div> 
-
-      <div class="cd-timeline-content">
-            <h3 style={{ color: 'white' }}>Event 4</h3>
-                <div class="timeline-content-info">
-          
-          <span class="timeline-content-info-date">
-            <i class="fa fa-calendar-o" aria-hidden="true"></i>
-            Nov 2020 - Present
-          </span>
-        </div>
-        <h2>desc</h2>
-        
-      </div> 
-    </div> 
-
-    <div class="cd-timeline-block">
-      <div class="cd-timeline-img cd-location">
-      </div> 
-
-      <div class="cd-timeline-content">
-            <h3 style={{ color: 'white' }}>Event 5</h3>
-            <div class="timeline-content-info">
-          
-          <span class="timeline-content-info-date">
-            <i class="fa fa-calendar-o" aria-hidden="true"></i>
-            Nov 2020 - Present
-          </span>
-        </div>
-        <h2>desc</h2>
-        
-      </div> 
-    </div> 
-
-    
-  </section> 
-  </div>
     );
   }
 }
