0596b82d2ed5201eb3b062a3ef967048d98b106f
aa-deet-eeya <aditya99mondal@gmail.com>
2020-10-29 12:52:44 +0530

    fixed failing Test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ReactGA from 'react-ga';
+// import ReactGA from 'react-ga';
 import $ from 'jquery';
 import './App.css';
 import Header from './Components/Header';
@@ -17,8 +17,8 @@ class App extends Component {
       resumeData: {},
     };
 
-    ReactGA.initialize('UA-110570651-1');
-    ReactGA.pageview(window.location.pathname);
+    // ReactGA.initialize('UA-110570651-1');
+    // ReactGA.pageview(window.location.pathname);
   }
 
   getResumeData() {
